a8f57d5972f1982b336c926f1f76b22e3788d17f
Praveenkumargr <136220341+Praveenkumargr@users.noreply.github.com>
2023-11-15 15:35:30 +0530

    Add files via upload

---
+++ helloform2/src/main/java/HelloForm2/HelloForm2.java
@@ -0,0 +1,61 @@
+package HelloForm2;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.*;
+
+/**
+ * Servlet implementation class HelloForm2
+ */
+public class HelloForm2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloForm2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+
+	      PrintWriter out = response.getWriter();
+	      String title = "Using GET Method to Read Form Data";
+	      String docType =
+	         "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+	         
+	      out.println(docType +
+	         "<html>\n" +
+	            "<head><title>" + title + "</title></head>\n" +
+	            "<body bgcolor = \"#f0f0f0\">\n" +
+	               "<h1 align = \"center\">" + title + "</h1>\n" +
+	               "<ul>\n" +
+	                  "  <li><b>First Name</b>: "
+	                  + request.getParameter("first_name") + "\n" +
+	                  "  <li><b>Last Name</b>: "
+	                  + request.getParameter("last_name") + "\n" +
+	               "</ul>\n" +
+	            "</body>" +
+	         "</html>"
+	            );
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
